0dbab5d7db46db223d4ad6012a012aa5ba559e70
haruki7049 <tontonkirikiri@gmail.com>
2025-01-17 22:25:24 +0900

    feat(repo): Init rust

---
+++ flake.nix
@@ -0,0 +1,111 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    crane.url = "github:ipetkov/crane";
+    flake-compat.url = "github:edolstra/flake-compat";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain rust;
+          overlays = [ inputs.rust-overlay.overlays.default ];
+          src = craneLib.cleanCargoSource ./.;
+          cargoArtifacts = craneLib.buildDepsOnly {
+            inherit src;
+          };
+          jec-subject = craneLib.buildPackage {
+            inherit src cargoArtifacts;
+            strictDeps = true;
+
+            doCheck = true;
+          };
+          cargo-clippy = craneLib.cargoClippy {
+            inherit src cargoArtifacts;
+            cargoClippyExtraArgs = "--verbose -- --deny warning";
+          };
+          cargo-doc = craneLib.cargoDoc {
+            inherit src cargoArtifacts;
+          };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system overlays;
+          };
+
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.rustfmt.enable = true;
+            #programs.taplo.enable = true; # BUG: `nix flake check --all-systems` failed on aarch64-darwin
+            programs.actionlint.enable = true;
+            programs.mdformat.enable = true;
+
+            settings = {
+              excludes = [
+                "LICENSE"
+                ".gitignore"
+                "Cargo.lock"
+                "flake.lock"
+              ];
+            };
+          };
+
+          packages = {
+            inherit jec-subject;
+            default = jec-subject;
+            doc = cargo-doc;
+          };
+
+          checks = {
+            inherit
+              jec-subject
+              cargo-clippy
+              cargo-doc
+              ;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              # Rust
+              rust
+
+              # Nix
+              pkgs.nil
+            ];
+
+            shellHook = ''
+              export PS1="\n[nix-shell:\w]$ "
+            '';
+          };
+        };
+    };
+}
